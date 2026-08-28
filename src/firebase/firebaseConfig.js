e1f3fb918bd5bf757799145c16a75ad6785a64b3
Jair <angeljair354@gmail.com>
2026-08-27 20:58:20 -0600

    Update

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDSMUT1TlbAUJrdpZ0OUl-DHm2GSTHbsdM",
-    authDomain: "herreritaxdd.firebaseapp.com",
-    databaseURL: "https://herreritaxdd-default-rtdb.firebaseio.com",
-    projectId: "herreritaxdd",
-    storageBucket: "herreritaxdd.firebasestorage.app",
-    messagingSenderId: "835892303245",
-    appId: "1:835892303245:web:aab58df54850828dd759c4"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
